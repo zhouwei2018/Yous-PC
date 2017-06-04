f59a86a3a27e0b2c61ac63410f96693037949c1b
zhouwei <zhouwei163mail@163.com>
2017-06-04 21:19:48 +0800

    修改api地址

--- www/main.js
+++ www/main.js
@@ -15,7 +15,7 @@ Vue.use(VueResource);
 Vue.use(iView, {locale});
 
 //const变量
-Vue.prototype.$api = "http://106.14.27.89:8001/api/GetServiceApiResult" //api地址
+Vue.prototype.$api = "http://116.62.71.76:8001/api/GetServiceApiResult" //api地址
 Vue.prototype.$resouceUrl = "http://localhost:8081/"  //资源文件地址
 
 // 开启debug模式
