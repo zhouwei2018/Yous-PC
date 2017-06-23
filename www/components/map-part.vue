fc325af702a4cf1018ecf4debee742c5af977ca4
heyong <heyong@cmcm.com>
2017-06-23 11:06:02 +0800

    地图二级搜索添加

--- www/components/map-part.vue
+++ www/components/map-part.vue
@@ -1,10 +1,14 @@
 <template>
     <div map-part>
+       <!-- <h2 class="fl clear"><i class="detail-icon supporting"></i>{{buildName}}</h2>-->
         <div class="map-out clear">
             <div id="mapId"></div>
             <div class="search-part">
                 <ul class="tab-nav">
-                    <li v-for="(item, index) in serviceItems" @click="searchHandler(index, item.dis)" :data-code="item.code" :data-key="item.dis" class="map-type-item" :class="{on:item.isActive }"><i></i>{{ item.title }}</li>
+                    <li v-for="(item, index) in serviceItems" @click="searchHandler(index, item.dis)" :data-code="item.code" :data-key="item.dis"  :class="[mapData.mainIndex==index? 'on' : '','map-type-item']"><i></i>{{ item.title }}</li>
+                </ul>
+                <ul class="sub-tab clear">
+                    <li class="result-item" v-for="(item_, index_) in mapData.subTypes" :class="mapData.subIndex==index_? 'currenttype' : ''"  @click="searchHandler_(index_, item_)">{{item_}}</li>
                 </ul>
                 <ul class="result-ul">
                     <li class="result-item" v-for="(item, index) in mapData.results"><i>{{ index+1 }}</i><p>{{ item.title }}{{item.content}}</p><span>{{ item.distance }}</span></li>
@@ -29,20 +33,24 @@
                     circle: null,
                     defaultcenterdpoint: null,
                     results: [],
+                    subTypes: [],
+                    mainIndex:0,
+                    subIndex:0,
                     markers:[],
                     currentinfobox: null,
                 },
+                buildName:"望京SOHO周边配套",
                 serviceItems:[
-                    {code:"jt", dis:"地铁,公交,停车场,加油站", title:"交通", isActive:1},
-                    {code:"cy", dis:"中餐,西餐,快餐,咖啡厅,茶座", title:"餐饮", isActive:0},
-                    {code:"jd", dis:"酒店，餐馆，星级酒店,快捷酒店,公寓式酒店", title:"酒店", isActive:0},
-                    {code:"js", dis:"体育场馆,极限运动场所,健身中心", title:"健身", isActive:0},
-                    {code:"jr", dis:"银行,ATM,投资理财", title:"金融", active:0},
+                    {code:"jt", dis:"地铁,公交,停车场,加油站", title:"交通"},
+                    {code:"cy", dis:"中餐,西餐,快餐,咖啡厅,茶座", title:"餐饮"},
+                    {code:"jd", dis:"星级酒店,快捷酒店,公寓式酒店", title:"酒店"},
+                    {code:"js", dis:"体育场馆,极限运动场所,健身中心", title:"健身"},
+                    {code:"jr", dis:"银行,ATM,投资理财", title:"金融"},
                 ],
             };
         },
         mounted: function () {
-               this.init();
+            this.init();
         },
         methods: {
             init:function(){
@@ -80,7 +88,7 @@
                 map.addOverlay(circle);
                 map.addOverlay(markerCenter);
                 this.addclickhandler(markerCenter);
-                this.searchHandler(0);
+                this.searchHandler(0, this.serviceItems[0]['dis']);
 
             },
             addcontrol: function(){		// 添加平移缩放控件
@@ -130,17 +138,17 @@
             addclickhandler: function (marker) {
                 var this_= this;
                 marker.addEventListener("mouseover", function (e) {
-                    var opts = {
-                        width : 200,     // 信息窗口宽度
-                        height: 100,     // 信息窗口高度
-                        title :marker['title'] , // 信息窗口标题
-                        enableMessage:true,//设置允许信息窗发送短息
-                        message:''
-                    }
-                    var infoWindow = new BMap.InfoWindow(marker['address'], opts);
-                    var p = marker.getPosition();
-                    var point = new BMap.Point(p['lng'],p['lat']);
-                    this_.mapData.map.openInfoWindow(infoWindow,point); //开启信息窗口
+                        var opts = {
+                            width : 200,     // 信息窗口宽度
+                            height: 100,     // 信息窗口高度
+                            title :marker['title'] , // 信息窗口标题
+                            enableMessage:true,//设置允许信息窗发送短息
+                            message:''
+                        }
+                        var infoWindow = new BMap.InfoWindow(marker['address'], opts);
+                        var p = marker.getPosition();
+                        var point = new BMap.Point(p['lng'],p['lat']);
+                        this_.mapData.map.openInfoWindow(infoWindow,point); //开启信息窗口
                     }
                 );
             },
@@ -150,19 +158,23 @@
                 }
                 enr.mapData.markers = [];
             },
-            searchHandler:function(index){
+            searchHandler:function(index, desc){
                 var this_ = this;
-                for(var  i=0,len=this.serviceItems.length;i<len;i++){
-                    this.serviceItems[i].isActive=0;
-                }
-                this.serviceItems[index].isActive=1;
+                this_.mapData.mainIndex = index;
+                this_.mapData.subIndex = 0;
+                this_.mapData.subTypes = desc.split(',');
+                this.searchHandler_(0, this_.mapData.subTypes[0]);
+            },
+            searchHandler_:function(a, b){
+                var this_ = this;
+                this.mapData.subIndex = a;
                 if (this.mapData.local != null) {
                     this.mapData.local.clearResults();
                 } else {
                     this.mapData.local = new BMap.LocalSearch(this.mapData.map, {});
                 }
 
-                this.mapData.local.searchNearby(this.serviceItems[index].title, this.mapData.point, 1000);
+                this.mapData.local.searchNearby(b, this.mapData.point, 1000);
                 this.mapData.local.setSearchCompleteCallback(function (r) {
                     var s = [];
                     for (var i = 0; i < r.getCurrentNumPois(); i++) {
@@ -176,7 +188,7 @@
                     this_.clearmarkers(this_);
                     this_.addmarkers(s,this_);
                 });
-            }
+            },
         }
     }
 
@@ -186,20 +198,29 @@
     .clear{
         clear: both;
         &:after{
-                content:".";
-                clear:both;
-                display:block;
-                height:0;
-                overflow:hidden;
-                visibility:hidden;
-            }
+            content:".";
+            clear:both;
+            display:block;
+            height:0;
+            overflow:hidden;
+            visibility:hidden;
+        }
     }
     [map-part]{
         .build-name{
             font-size: 20px;
             text-align: center;
-            &before{
-
+            i{
+                .detail-icon {
+                    float: left;
+                    top: 0;
+                    width: 30px;
+                    height: 30px;
+                    margin-right: 10px;
+                    display: inline-block;
+                    background: url("../resources/images/icons/detail-icon-bg.png") no-repeat;
+                    background-position: -106px -220px;
+                }
             }
         }
         .map-out{
@@ -211,122 +232,140 @@
                 width: 800px;
                 height:420px;
             }
-           .search-part{
+            .search-part{
                 float: right;
                 width: 380px;
                 height: 420px;
                 border: 1px solid #EEE;
                 box-sizing: border-box;
-               .tab-nav{
-                   height: 56px;
-                   border-bottom: 1px solid #EEE;
-                   li{
-                       float: left;
-                       width: 20%;
-                       height: 100%;
-                       line-height: 21px;
-                       font-size: 13px;
-                       text-align: center;
-                       cursor: pointer;
-                       i{
-                           position: relative;
-                           top: 8px;
-                           display: block;
-                           width: 26px;
-                           height: 26px;
-                           margin: 0 auto 4px;
-                           background: url("../resources/images/icons/detail-icon-bg.png") no-repeat;
-                       }
-                   }
-                   li.on{
-                       color: #FFF;
-                       background-color: #302F35;
-                   }
-                   li:nth-child(1){
-                       i{
-                           background-position: -27px -148px;
-                       }
-                   }
-                   li.on:nth-child(1){
-                       i{
-                           background-position: -27px -117px;
-                       }
-                   }
-                   li:nth-child(2){
-                       i{
-                           background-position: -228px -147px;
-                       }
-                   }
-                   li.on:nth-child(2){
-                       i{
-                           background-position: -228px -116px;
-                       }
-                   }
-                   li:nth-child(3){
-                       i{
-                           background-position: -188px -146px;
-                       }
-                   }
-                   li.on:nth-child(3){
-                       i{
-                           background-position: -188px -115px;
-                       }
-                   }
-                   li:nth-child(4){
-                       i{
-                           background-position: -108px -147px;
-                       }
-                   }
-                   li.on:nth-child(4){
-                       i{
-                           background-position: -108px -116px;
-                       }
-                   }
-                   li:nth-child(5){
-                       i{
-                           background-position: -148px -146px;
-                       }
-                   }
-                   li.on:nth-child(5){
-                       i{
-                           background-position: -148px -115px;
-                       }
-                   }
-               }
-               .result-ul{
-                   height: 316px;
-                   overflow-x: hidden;
-                   overflow-y: auto;
-                   .result-item{
-                       height: 20px;
-                       line-height: 20px;
-                       padding: 10px 20px 10px 15px;
-                       font-size: 13px;
-                       cursor: pointer;
-                       box-sizing: content-box;
-                       i{
-                           display: block;
-                           float: left;
-                           width: 20px;
-                           height: 20px;
-                           margin-right: 7px;
-                           color: #FFF;
-                           text-align: center;
-                           border-radius: 50%;
-                           background-color: #302F35;
-                       }
-                       p{
-                           float: left;
-                           width: 240px;
-                           white-space: nowrap;
-                           overflow: hidden;
-                           text-overflow: ellipsis;
-                       }
-                       span{
-                           float: right;
-                       }
-                   }
-               }
+                .tab-nav{
+                    height: 56px;
+                    border-bottom: 1px solid #EEE;
+                    li{
+                        float: left;
+                        width: 20%;
+                        height: 100%;
+                        line-height: 21px;
+                        font-size: 13px;
+                        text-align: center;
+                        cursor: pointer;
+                        i{
+                            position: relative;
+                            top: 8px;
+                            display: block;
+                            width: 26px;
+                            height: 26px;
+                            margin: 0 auto 4px;
+                            background: url("../resources/images/icons/detail-icon-bg.png") no-repeat;
+                        }
+                    }
+                    li.on{
+                        color: #FFF;
+                        background-color: #302F35;
+                    }
+                    li:nth-child(1){
+                        i{
+                            background-position: -27px -148px;
+                        }
+                    }
+                    li.on:nth-child(1){
+                        i{
+                            background-position: -27px -117px;
+                        }
+                    }
+                    li:nth-child(2){
+                        i{
+                            background-position: -228px -147px;
+                        }
+                    }
+                    li.on:nth-child(2){
+                        i{
+                            background-position: -228px -116px;
+                        }
+                    }
+                    li:nth-child(3){
+                        i{
+                            background-position: -188px -146px;
+                        }
+                    }
+                    li.on:nth-child(3){
+                        i{
+                            background-position: -188px -115px;
+                        }
+                    }
+                    li:nth-child(4){
+                        i{
+                            background-position: -108px -147px;
+                        }
+                    }
+                    li.on:nth-child(4){
+                        i{
+                            background-position: -108px -116px;
+                        }
+                    }
+                    li:nth-child(5){
+                        i{
+                            background-position: -148px -146px;
+                        }
+                    }
+                    li.on:nth-child(5){
+                        i{
+                            background-position: -148px -115px;
+                        }
+                    }
+                }
+                .sub-tab{
+                    height: 44px;
+                    border-bottom: 1px solid #EEE;
+                    li {
+                        float: left;
+                        min-width: 70px;
+                        height: 44px;
+                        line-height: 18px;
+                        padding: 14px 10px 13px;
+                        font-size: 13px;
+                        text-align: center;
+                        box-sizing: border-box;
+                        cursor: pointer;
+                    }
+                    li.currenttype {
+                        border-bottom: 3px solid #302F35;
+                    }
+                }
+                .result-ul{
+                    height: 316px;
+                    overflow-x: hidden;
+                    overflow-y: auto;
+                    .result-item{
+                        height: 20px;
+                        line-height: 20px;
+                        padding: 10px 20px 10px 15px;
+                        font-size: 13px;
+                        cursor: pointer;
+                        box-sizing: content-box;
+                        i{
+                            display: block;
+                            float: left;
+                            width: 20px;
+                            height: 20px;
+                            margin-right: 7px;
+                            color: #FFF;
+                            text-align: center;
+                            border-radius: 50%;
+                            background-color: #302F35;
+                        }
+                        p{
+                            float: left;
+                            width: 240px;
+                            white-space: nowrap;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                        }
+                        span{
+                            float: right;
+                        }
+                    }
+                }
             }
         }
     }
