83f1f6bdfec949589da17fadc83ab6765ebc0573
qizhenzhen <qizhenzhen2017@126.com>
2017-08-16 14:37:38 +0800

    160 楼盘详情----楼盘首页，点击免费回拨弹出信息框（请看附图）

--- www/routers/detail_component/build_index.vue
+++ www/routers/detail_component/build_index.vue
@@ -495,7 +495,7 @@
                     </div>
                     <p>您也可以拨打<i> 400-078-8800 </i>直接委托需求给幼狮</p>
                     <Form-item>
-                        <input type="primary" readonly class="pop_subbtn" value="提交" @click="handleSubmit2('formInline4')">
+                        <input type="primary" readonly class="pop_subbtn" value="提交" @click="handleSubmit3('formInline4')">
                     </Form-item>
                 </Form>
             </div>
