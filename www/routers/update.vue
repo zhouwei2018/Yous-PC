cf54f46db0476cf30dfed39ad7dc1cb4cded8639
qizhenzhen2017@126.com <haiou920113>
2017-08-27 18:35:06 +0800

    update

--- www/routers/update.vue
+++ www/routers/update.vue
@@ -8,258 +8,182 @@
         <header1></header1>
         <!--update start-->
         <div update>
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide slide1"></div>
-                    <div class="swiper-slide pr slide2">
-                        <div class="up_two_prag" id="animate_2">
-                            <p>企业升级服务部以提供专业化服务为宗旨，解决用户在发展过程中遇到的有碍其成长的资金、人力、风险控制等困扰因素为目标，以办公空间为载体，
-                                通过提供财税服务、法务服务、人力资源服务、商学院服务、FA服务等内容，与客户建立深度链接、超强黏性，进而带来新的办公空间需求，通过对空
-                                间租户的服务，衍生辐射到非空间用户，从而形成具有影响力的企业办公服务品牌，服务助力空间、空间承载服务，最终形成“有形的空间+无形的服务”
-                                一站式全新办公服务体验。
-                            </p>
-                        </div>
-                        <div class="update_icon_wrap" id="animate">
-                            <span><i class="up_icon1"></i>财税服务</span>
-                            <span><i class="up_icon2"></i>法务服务</span>
-                            <span><i class="up_icon3"></i>人力资源服务</span>
-                            <span><i class="up_icon4"></i>商学院服务</span>
-                            <span><i class="up_icon5"></i>FA服务</span>
-                        </div>
+            <div class="swiper-wrapper">
+                <div class="swiper-slide slide1">
+                    <img src="../resources/images/update/update.jpg" alt="">
+                </div>
+                <div class="swiper-slide pr slide2">
+                    <img src="../resources/images/update/update_01.png" alt="">
+                    <div class="up_two_prag" id="animate_2">
+                        <p>企业升级服务部以提供专业化服务为宗旨，解决用户在发展过程中遇到的有碍其成长的资金、人力、风险控制等困扰因素为目标，以办公空间为载体，
+                            通过提供财税服务、法务服务、人力资源服务、商学院服务、FA服务等内容，与客户建立深度链接、超强黏性，进而带来新的办公空间需求，通过对空
+                            间租户的服务，衍生辐射到非空间用户，从而形成具有影响力的企业办公服务品牌，服务助力空间、空间承载服务，最终形成“有形的空间+无形的服务”
+                            一站式全新办公服务体验。
+                        </p>
                     </div>
-                    <div class="swiper-slide slide3">
-                        <ul class="update_ser_wrap" id="animate2">
-                            <li class="clearfix">
+                    <div class="update_icon_wrap" id="animate">
+                        <span><i class="up_icon1"></i>财税服务</span>
+                        <span><i class="up_icon2"></i>法务服务</span>
+                        <span><i class="up_icon3"></i>人力资源服务</span>
+                        <span><i class="up_icon4"></i>商学院服务</span>
+                        <span><i class="up_icon5"></i>FA服务</span>
+                    </div>
+                </div>
+                <div class="swiper-slide slide3">
+                    <ul class="update_ser_wrap" id="animate2">
+                        <li class="clearfix">
                                 <span class="fl up_left_box">
                                     <i class="black">我的客服</i>
                                     <i>贴心专业，快速响应</i>
                                     <i>使企业更专注于自身业务</i>
                                 </span>
-                                <span class="fr up_ser_rtips ser1"></span>
-                            </li>
-                            <li class="clearfix">
+                            <span class="fr up_ser_rtips ser1"></span>
+                        </li>
+                        <li class="clearfix">
                                 <span class="fl up_left_box">
                                     <i class="black">我的服务</i>
                                     <i>线上下单，线下快速解决</i>
                                     <i>企业成长过程中核心需求</i>
                                 </span>
-                                <span class="fr up_ser_rtips ser2"></span>
-                            </li>
-                            <li class="clearfix">
+                            <span class="fr up_ser_rtips ser2"></span>
+                        </li>
+                        <li class="clearfix">
                                 <span class="fl up_left_box">
                                     <i class="black">我的动态</i>
                                     <i>发布企业动态，实现线上</i>
                                     <i>线下企业服务资源高效对接，构建企业生态圈</i>
                                 </span>
-                                <span class="fr up_ser_rtips ser3"></span>
-                            </li>
-                            <li class="clearfix">
+                            <span class="fr up_ser_rtips ser3"></span>
+                        </li>
+                        <li class="clearfix">
                                 <span class="fl up_left_box">
                                     <i class="black">我的钱包</i>
                                     <i>企业服务平台实</i>
                                     <i>现交易过程中的资金保障</i>
                                 </span>
-                                <span class="fr up_ser_rtips ser4"></span>
-                            </li>
-                        </ul>
-                        <img class="pad_icon" src="../resources/images/update/pad_icon.png" alt="">
-                        <div class="up_service_desc" id="animate3">
-                            <h3 class="mb10">企业升级服务平台</h3>
-                            <p>登录幼狮科技企业服务平台，您可以享受舒适的办公空间，结交优质的商业合作伙伴，获得高效的专属服务。</p>
+                            <span class="fr up_ser_rtips ser4"></span>
+                        </li>
+                    </ul>
+                    <img class="pad_icon" src="../resources/images/update/pad_icon.png" alt="">
+                    <div class="up_service_desc" id="animate3">
+                        <h3 class="mb10">企业升级服务平台</h3>
+                        <p>登录幼狮科技企业服务平台，您可以享受舒适的办公空间，结交优质的商业合作伙伴，获得高效的专属服务。</p>
+                    </div>
+                </div>
+                <div class="swiper-slide slide4">
+                    <div class="serice_four_right" id="animate4_1">
+                        <h3>财/法/税支持</h3>
+                        <p>数家知名会计事务所／律师事务所，针对不同行业，</p>
+                        <p>为企业提供更加细致、专业的财／法／税的咨询服务，</p>
+                        <p>把控企业风险，提供解决及优化方案。</p>
+                    </div>
+
+                    <div class="serice_four_left" id="animate4_2">
+                        <div class="service_four_item">
+                            <h3>finance 财务</h3>
+                            <p>优化中小企业销售费用管理结构</p>
+                            <p>企业财务风险管理存在的问题及解决策略</p>
+                        </div>
+                        <div class="service_four_item">
+                            <h3>legal affairs 法务</h3>
+                            <p>公司成立后，如何应对股东出资瑕疵/出资不足</p>
+                            <p>法律风险防范和业务发展的对立与冲突</p>
+                        </div>
+                        <div class="service_four_item">
+                            <h3>Taxation 税务</h3>
+                            <p>企业设立分公司时，办理税务登记的相关流程</p>
+                            <p>如何权衡纳税成本与企业发展之间制约关系</p>
                         </div>
                     </div>
-                    <div class="swiper-slide slide4">
-                        <div class="serice_four_right" id="animate4_1">
-                            <h3>财/法/税支持</h3>
-                            <p>数家知名会计事务所／律师事务所，针对不同行业，</p>
-                            <p>为企业提供更加细致、专业的财／法／税的咨询服务，</p>
-                            <p>把控企业风险，提供解决及优化方案。</p>
-                        </div>
 
-                        <div class="serice_four_left" id="animate4_2">
-                            <div class="service_four_item">
-                                <h3>finance 财务</h3>
-                                <p>优化中小企业销售费用管理结构</p>
-                                <p>企业财务风险管理存在的问题及解决策略</p>
-                            </div>
-                            <div class="service_four_item">
-                                <h3>legal affairs 法务</h3>
-                                <p>公司成立后，如何应对股东出资瑕疵/出资不足</p>
-                                <p>法律风险防范和业务发展的对立与冲突</p>
-                            </div>
-                            <div class="service_four_item">
-                                <h3>Taxation 税务</h3>
-                                <p>企业设立分公司时，办理税务登记的相关流程</p>
-                                <p>如何权衡纳税成本与企业发展之间制约关系</p>
-                            </div>
-                        </div>
+                    <img class="male_icon" src="../resources/images/update/male_icon.png" alt="">
 
-                        <img class="male_icon" src="../resources/images/update/male_icon.png" alt="">
+                </div>
+                <div class="swiper-slide slide5">
+                    <img class="light_icon" src="../resources/images/update/light_icon.png" alt="">
 
+                    <div class="serice_five_right" id="animate5_1">
+                        <h3 class="mb10">FA服务/投融资对接</h3>
+                        <p>深挖不同专业领域和投资方向的国内外投资人，普惠</p>
+                        <p>于幼狮空间企业，定期举办投资人见面会和项目路演</p>
+                        <p>为企业与投资人建立最直接、最深入的对接平台。</p>
                     </div>
-                    <div class="swiper-slide slide5">
-                        <img class="light_icon" src="../resources/images/update/light_icon.png" alt="">
-
-                        <div class="serice_five_right" id="animate5_1">
-                            <h3 class="mb10">FA服务/投融资对接</h3>
-                            <p>深挖不同专业领域和投资方向的国内外投资人，普惠</p>
-                            <p>于幼狮空间企业，定期举办投资人见面会和项目路演</p>
-                            <p>为企业与投资人建立最直接、最深入的对接平台。</p>
-                        </div>
-                        <div class="serice_five_left" id="animate5_2">
-                            <div class="service_four_item">
-                                <p>为个人投资者，幼狮空间企业，基金及银行借贷机构，提供优质投资项目。</p>
-                                <p>通过VC/PE机构打通融资渠道，迅速、高效解决企业资金需求。</p>
-                            </div>
+                    <div class="serice_five_left" id="animate5_2">
+                        <div class="service_four_item">
+                            <p>为个人投资者，幼狮空间企业，基金及银行借贷机构，提供优质投资项目。</p>
+                            <p>通过VC/PE机构打通融资渠道，迅速、高效解决企业资金需求。</p>
                         </div>
                     </div>
-                    <div class="swiper-slide slide6">
-                        <img class="female_icon" src="../resources/images/update/female_icon.png" alt="">
-                        <div class="serice_six" id="animate6">
-                            <h3 class="mb10">幼狮商学院</h3>
-                            <p>作为幼狮空间办公生态体系的重要组成部分，幼狮商学院将成为幼</p>
-                            <p>狮空间企业在战略执行和价值创造的道路上提供各类管理实践课程</p>
-                            <p>同时发起并展开论坛、讲座、沙龙等丰富的交流活动。</p>
-                        </div>
-                        <img class="four_icon" src="../resources/images/update/four_icon.png" alt="">
-                        <img class="hr_icon" src="../resources/images/update/hr_icon.png" alt="">
-                        <div class="serice_seven half" id="animate6_half">
-                            <h3 class="mb10">人力资源服务</h3>
-                            <p>根据企业发展战略的要求，及对人才的需求</p>
-                            <p>为企业提供人才服务，从而促进企业人力资</p>
-                            <p>源的有效开发与优化配置</p>
-                        </div>
+                </div>
+                <div class="swiper-slide slide6">
+                    <img class="female_icon" src="../resources/images/update/female_icon.png" alt="">
+                    <div class="serice_six" id="animate6">
+                        <h3 class="mb10">幼狮商学院</h3>
+                        <p>作为幼狮空间办公生态体系的重要组成部分，幼狮商学院将成为幼</p>
+                        <p>狮空间企业在战略执行和价值创造的道路上提供各类管理实践课程</p>
+                        <p>同时发起并展开论坛、讲座、沙龙等丰富的交流活动。</p>
                     </div>
-                    <div class="swiper-slide slide7">
-                        <div class="serice_seven" id="animate7">
-                            <h3 class="mb10">人力资源服务</h3>
-                            <p>根据企业发展战略的要求，及对人才的需求</p>
-                            <p>为企业提供人才服务，从而促进企业人力资</p>
-                            <p>源的有效开发与优化配置</p>
-                        </div>
-                        <img class="hr_icon_1" src="../resources/images/update/hr_icon.png" alt="">
+                    <img class="four_icon" src="../resources/images/update/four_icon.png" alt="">
+                    <img class="hr_icon" src="../resources/images/update/hr_icon.png" alt="">
+                    <div class="serice_seven half" id="animate6_half">
+                        <h3 class="mb10">人力资源服务</h3>
+                        <p>根据企业发展战略的要求，及对人才的需求</p>
+                        <p>为企业提供人才服务，从而促进企业人力资</p>
+                        <p>源的有效开发与优化配置</p>
+                    </div>
+                </div>
+                <div class="swiper-slide slide7">
+                    <div class="serice_seven" id="animate7">
+                        <h3 class="mb10">人力资源服务</h3>
+                        <p>根据企业发展战略的要求，及对人才的需求</p>
+                        <p>为企业提供人才服务，从而促进企业人力资</p>
+                        <p>源的有效开发与优化配置</p>
+                    </div>
+                    <img class="hr_icon_1" src="../resources/images/update/hr_icon.png" alt="">
 
-                        <div footer>
-                            <div class="contents foot_head clearfix">
-                                <div class="fl">
-                                    <h3>北京幼狮科技有限公司</h3>
-                                    <p><i class="commerce_tel"></i>400-078-8800</p>
-                                    <p class="lion_address">地址：北京市朝阳区东三环中路39号建外SOHO东区A座2503</p>
-                                </div>
+                    <div footer>
+                        <div class="contents foot_head clearfix">
+                            <div class="fl">
+                                <h3>北京幼狮科技有限公司</h3>
+                                <p><i class="commerce_tel"></i>400-078-8800</p>
+                                <p class="lion_address">地址：北京市朝阳区东三环中路39号建外SOHO东区A座2503</p>
+                            </div>
+                            <div class="fr">
+                                <img src="../resources/images/ys_weixin.jpg" alt="幼狮微信"
+                                     style="width: 98px;height: 98px;"/>
+                                <span class="weixin_text">幼狮官网</span>
+                            </div>
+                        </div>
+                        <div class="foot_bot">
+                            <div class="contents">
+                                <span class="bottom_copyright fl">北京幼狮科技有限公司 保留所有权利&nbsp;&nbsp;京ICP备20176444</span>
                                 <div class="fr">
-                                    <img src="../resources/images/ys_weixin.jpg" alt="幼狮微信"
-                                         style="width: 98px;height: 98px;"/>
-                                    <span class="weixin_text">幼狮官网</span>
-                                </div>
-                            </div>
-                            <div class="foot_bot">
-                                <div class="contents">
-                                    <span class="bottom_copyright fl">北京幼狮科技有限公司 保留所有权利&nbsp;&nbsp;京ICP备20176444</span>
-                                    <div class="fr">
                                       <span>
                                           <router-link :to="{ path: '/about',query:{name:0}}">关于我们</router-link>
                                       </span>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination"></div>
             </div>
-
         </div>
 
+        <!--footer-->
+        <footer1></footer1>
     </div>
 
 </template>
 <script>
     import header1 from '../components/header.vue';
-
-    import '../resources/plugin/swiper/js/swiper.min.js';
+    import footer1 from '../components/footer.vue';
 
     export default {
-        components: {header1},
+        components: {header1, footer1},
         mounted(){
             $(window).scrollTop(0);
 
-            //banner swiper
-            new Swiper('.swiper-container', {
-                pagination: '.swiper-pagination', //分页器
-                direction: 'vertical',
-                slidesPerView: 1,
-                paginationClickable: true,
-                mousewheelControl: true,
-                onScroll: function (swiper) {
-                    if (swiper.activeIndex == 1) {
-                        $('#animate').addClass('animated infinite bounceInUp');
-                        setTimeout(function () {
-                            $('#animate').removeClass('animated infinite bounceInUp')
-                        }, 1000);
 
-                        $('#animate_2').addClass('animated infinite swing');
-                        setTimeout(function () {
-                            $('#animate_2').removeClass('animated infinite swing')
-                        }, 1000);
-                    }
-
-                    if (swiper.activeIndex == 2) {
-                        $('#animate2').addClass('animated infinite bounceInLeft');
-                        setTimeout(function () {
-                            $('#animate2').removeClass('animated infinite bounceInLeft')
-                        }, 1000);
-
-                        $('#animate3').addClass('animated infinite bounceInUp');
-                        setTimeout(function () {
-                            $('#animate3').removeClass('animated infinite bounceInUp')
-                        }, 1000);
-                    }
-
-                    if (swiper.activeIndex == 3) {
-                        $('#animate4_1').addClass('animated infinite fadeInRight');
-                        setTimeout(function () {
-                            $('#animate4_1').removeClass('animated infinite fadeInRight')
-                        }, 1000);
-
-                        $('#animate4_2').addClass('animated infinite fadeInUp');
-                        setTimeout(function () {
-                            $('#animate4_2').removeClass('animated infinite fadeInUp')
-                        }, 1000);
-                    }
-
-                    if (swiper.activeIndex == 4) {
-                        $('#animate5_1').addClass('animated infinite flipInY');
-                        setTimeout(function () {
-                            $('#animate5_1').removeClass('animated infinite flipInY')
-                        }, 1000);
-
-                        $('#animate5_2').addClass('animated infinite fadeInUpBig');
-                        setTimeout(function () {
-                            $('#animate5_2').removeClass('animated infinite fadeInUpBig')
-                        }, 1000);
-                    }
-
-                    if (swiper.activeIndex == 5) {
-                        $('#animate6').addClass('animated infinite fadeInRight');
-                        setTimeout(function () {
-                            $('#animate6').removeClass('animated infinite fadeInRight')
-                        }, 1000);
-
-                        $('#animate6_half').addClass('animated infinite fadeInRight');
-                        setTimeout(function () {
-                            $('#animate6_half').removeClass('animated infinite fadeInRight')
-                        }, 1000);
-
-                    }
-                    if (swiper.activeIndex == 6) {
-                        $('#animate7').addClass('animated infinite fadeInUp');
-                        setTimeout(function () {
-                            $('#animate7').removeClass('animated infinite fadeInUp')
-                        }, 1000);
-                    }
-                }
-            })
         }
     }
 </script>
